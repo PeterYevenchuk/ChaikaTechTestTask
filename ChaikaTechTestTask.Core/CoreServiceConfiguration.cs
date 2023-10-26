365e60d9456372a0f9effff03d2c56d338a9f780
Peter <90260867+PeterYevenchuk@users.noreply.github.com>
2023-10-26 20:50:16 +0300

    add mediatr DI

---
+++ ChaikaTechTestTask.Core/CoreServiceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ChaikaTechTestTask.Core
+{
+    public class CoreServiceConfiguration
+    {
+        public static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            return services
+                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CoreServiceConfiguration).Assembly));
+        }
+    }
+}
--- ChaikaTechTestTask.WebAPI/Program.cs
+++ ChaikaTechTestTask.WebAPI/Program.cs
@@ -1,15 +1,17 @@
+using ChaikaTechTestTask.Core;
 using ChaikaTechTestTask.Core.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+CoreServiceConfiguration.ConfigureServices(builder.Services);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ChaikaTechDbContext>(
-    d => d.UseNpgsql(builder.Configuration.GetConnectionString("SqlConnectionString"))
-    );
+    d => d.UseNpgsql(builder.Configuration.GetConnectionString("SqlConnectionString")));
 
 var app = builder.Build();
 
